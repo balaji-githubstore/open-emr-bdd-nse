6225682fde61176b6009d7e0cf39e958218010ad
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-11 19:57:24 +0530

    two menu page methods added

--- src/test/java/com/nse/pages/MainPage.java
+++ src/test/java/com/nse/pages/MainPage.java
@@ -1,12 +1,23 @@
 package com.nse.pages;
 
+import org.openqa.selenium.By;
+
 import com.nse.base.WebDriverWrapper;
 
 public class MainPage {
-
+	private static By patientLocator=By.xpath("//*[contains(text(),'Patient')]");
+	private static By newSearchLocator=By.xpath("//*[contains(text(),'New/Se')]");
 	
 	public static String getDashboardPageTitle()
 	{
 		return WebDriverWrapper.driver.getTitle();
+	}
+	
+	public static void clickOnPatient() {
+		WebDriverWrapper.driver.findElement(patientLocator).click();
+	}
+	
+	public static void clickOnNewSearch() {
+		WebDriverWrapper.driver.findElement(newSearchLocator).click();
 	}
 }
